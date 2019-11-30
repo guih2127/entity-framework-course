b478e35e8b3cdf6c89e4e6a35b577f85b0454c09
Guilherme Reis Araujo <guih2127@gmail.com>
2019-11-30 15:32:02 -0300

    Conclusão da primeira parte - Introdução ao Entity Framework Core

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    // Nossa classe de contexto será utilizada para persistir todas as classes que devem ser persistidas
+    // pela aplicação. Ela deve fazer 3 coisas:
+    // 1 - Permitir que seja utilizada dentro dela a API do EF Core. (Herdamos de DbContext, do EntityFrameworkCore)
+    // 2 - Devemos informar as classes a serem persistidas pelo EF Core (Aqui, por enquanto, a classe Produto)
+    // 3 - A classe deve definir também qual será o banco de dados, fazemos isso no evento de configuração da classe, sobrescrevendo 
+    // o metódo OnConfiguring da classe DbContext
+
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public LojaContext()
+        { }
+
+        public LojaContext(DbContextOptions<LojaContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            }
+        }
+
+        // Adicionamos construtores na nossa classe LojaContext, que tornam a classe independente do banco de dados empregados.
+        // Assim, passando as opções necessárias para utilizar o MySQL, chamamos o construtor da classe base DbContext.
+        // Por último, o metódo OnConfiguring() deve utilizar o SQLServer apenas se as opções ainda não estiverem sido configuradas.
+    }
+}
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,7 +10,34 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p1 = new Produto();
+            p1.Nome = "Harry Potter e a Ordem da Fênix";
+            p1.Categoria = "Livros";
+            p1.Preco = 19.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "Senhor dos Anéis 1";
+            p2.Categoria = "Livros";
+            p2.Preco = 19.89;
+
+            Produto p3 = new Produto();
+            p3.Nome = "O Monge e o Executivo";
+            p3.Categoria = "Livros";
+            p3.Preco = 19.89;
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.AddRange(p1, p2, p3);
+                contexto.SaveChanges();
+            }
+            // Com o metódo AddRange(), podemos adicionar vários objetos de uma vez no metódo. Além disso,
+            // ganhamos uma melhora de performance para a inclusão de vários objetos.
         }
 
         private static void GravarUsandoAdoNet()
@@ -25,5 +52,8 @@ namespace Alura.Loja.Testes.ConsoleApp
                 repo.Adicionar(p);
             }
         }
+        // O metódo de gravar usando ADO usa como estratégia a criação de uma classe que representa o DAO
+        // da classe a ser persistida (no caso, ProdutoDAO). No Entity, utilizamos uma classe que persista todas
+        // as classes que devem ser persistidas, não apenas Produto. Essa classe é nomeada como Contexto da aplicação.
     }
 }
