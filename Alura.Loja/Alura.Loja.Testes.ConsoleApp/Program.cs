5dc44712e7fe674563a3789dccebd5c98e0d0d9b
Guilherme Reis Araujo <guih2127@gmail.com>
2019-12-01 20:39:18 -0300

    Primeiras aulas da Aula 03

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,84 +13,37 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            RecuperarProdutos();
-            ExcluirProdutos();
-            RecuperarProdutos();
-            AtualizarProduto();
-        }
-
-        private static void AtualizarProduto()
-        {
-            // Incluir um produto.
-            GravarUsandoEntity();
-
-            // Obter o produto e atualiza-lo.
-            using (var repo = new ProdutoDAOEntity())
+            using (var contexto = new LojaContext())
             {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Cassino Royale - Editado";
-                repo.Atualizar(primeiro);
-            }
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-            RecuperarProdutos();
-        }
+                var produtos = contexto.Produtos.ToList();
 
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach(var item in produtos)
-                {
-                    repo.Remover(item);
-                }
+                foreach (var p in produtos)
+                    Console.WriteLine(p);
+
+                Console.WriteLine(" ---------- ");
+
+                foreach (var e in contexto.ChangeTracker.Entries())
+                    Console.WriteLine(e.State);
+
+                var p1 = produtos.First();
+                p1.Nome = "Harry Potter - 22";
+
+                foreach (var e in contexto.ChangeTracker.Entries())
+                    Console.WriteLine(e.State);
+
+                contexto.SaveChanges();
+
+                Console.WriteLine(" ---------- ");
+
+                produtos = contexto.Produtos.ToList();
+
+                foreach (var p in produtos)
+                    Console.WriteLine(p);
             }
         }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produtos.", produtos.Count);
-                foreach(var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
-
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                contexto.Adicionar(p);
-            }
-            // Com o metódo AddRange(), podemos adicionar vários objetos de uma vez no metódo. Além disso,
-            // ganhamos uma melhora de performance para a inclusão de vários objetos.
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
-        // O metódo de gravar usando ADO usa como estratégia a criação de uma classe que representa o DAO
-        // da classe a ser persistida (no caso, ProdutoDAO). No Entity, utilizamos uma classe que persista todas
-        // as classes que devem ser persistidas, não apenas Produto. Essa classe é nomeada como Contexto da aplicação.
     }
 }
